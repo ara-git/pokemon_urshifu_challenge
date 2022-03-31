9b7d8791e704db368e6d7304e67089ed3004d272
ara-git <baribari.sanda@gmail.com>
2022-03-31 18:17:17 +0900

    update src:covert one-hot

--- kedro-project/src/kedro_project/pipelines/preprocess/node_preprocess.py
+++ kedro-project/src/kedro_project/pipelines/preprocess/node_preprocess.py
@@ -1,4 +1,52 @@
 import pandas as pd
 
-def preprocess(dark_urshifu_data, water_urshifu_data, frequent_pokemon_data):
-    return pd.DataFrame([1,2])
+
+def merge_data(dark_urshifu_data, water_urshifu_data):
+    """
+    悪、水ウーラオスのデータを結合する
+    """
+    merged_df = pd.concat([dark_urshifu_data, water_urshifu_data])
+    return merged_df
+
+
+def convert_one_hot(merged_df, frequent_pokemon_data):
+    """
+    使用したポケモンに関する、シンプルなone-hotデータに変換する。
+    ただし、低頻出（一回以下）ポケモンのデータは使わない。
+    
+    Augs
+        merged_data:悪、水ウーラオスのパーティデータ
+        frequent_pokemon_data:高頻出（2回以上）ポケモンの名前が入ったDataFrame
+    Returns
+        one_hot_df:パーティで使用しているポケモンの情報のみが入ったシンプルなone-hot
+    """
+    # 高頻出ポケモンのリストを作成し、それに関する辞書を作成する    
+    frequent_list = list(frequent_pokemon_data["name"])
+    one_hot_dict = dict(zip(frequent_list, [0] * len(frequent_list)))
+
+    one_hot_list = []
+    ## DataFrameを一行ずつ取り出す
+    for raw in merged_df.itertuples():
+        #辞書の値を０に戻す
+        one_hot_dict = one_hot_dict.fromkeys(frequent_list, 0)
+
+        for i in range(6):
+            poke_name = raw[i + 1]
+            if poke_name in one_hot_dict.keys():
+                # キーが存在する（ある程度頻出）のポケモンのみ抽出する
+                one_hot_dict[poke_name] = 1
+
+        one_hot_list.append(list(one_hot_dict.values()))
+
+    one_hot_df = pd.DataFrame(one_hot_list, columns = frequent_list)
+
+    # 列名を変更する
+    one_hot_df = one_hot_df.rename(columns={"ウーラオス悪": "target"})
+    # 不要列を削除する
+    del one_hot_df["ウーラオス水"]
+
+    return one_hot_df    
+
+"""
+def preprocess(merged_df, frequent_pokemon_data):
+"""
--- kedro-project/src/kedro_project/pipelines/preprocess/pipeline.py
+++ kedro-project/src/kedro_project/pipelines/preprocess/pipeline.py
@@ -6,21 +6,28 @@ Delete this when you start working on your own Kedro project.
 
 from kedro.pipeline import Pipeline, node
 
-from .node_preprocess import preprocess
 from .node_make_frequent_pokemon_data import make_frequent_pokemon_data
+from .node_preprocess import merge_data
+from .node_preprocess import convert_one_hot
+
 
 def create_pipeline(**kwargs):
     return Pipeline(
         [
             node(
-                make_frequent_pokemon_data,
+                merge_data,
                 ["raw_dark_urshifu_data", "raw_water_urshifu_data"],
+                "intermediate_merged_data"
+            ), 
+            node(
+                make_frequent_pokemon_data,
+                ["intermediate_merged_data", "params:param_freq_threshold"],
                 "intermediate_frequent_pokemon_data"
             ),
             node(
-                preprocess,
-                ["raw_dark_urshifu_data", "raw_water_urshifu_data", "intermediate_frequent_pokemon_data"],
-                "intermediate_preprocessed_data"
+                convert_one_hot,
+                ["intermediate_merged_data", "intermediate_frequent_pokemon_data"],
+                "primary_one_hot_data"
             ),
         ]
     )
