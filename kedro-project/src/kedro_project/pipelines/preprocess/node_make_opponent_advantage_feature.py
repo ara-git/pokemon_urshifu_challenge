50d9c3e9bfaba0c2fbf806a2fc48db7810fc5add
ara-git <baribari.sanda@gmail.com>
2022-04-07 01:33:07 +0900

    change opponent advantage logic

--- kedro-project/src/kedro_project/pipelines/preprocess/node_make_opponent_advantage_feature.py
+++ kedro-project/src/kedro_project/pipelines/preprocess/node_make_opponent_advantage_feature.py
@@ -4,7 +4,7 @@ import numpy as np
 def make_opponent_advantage_features(type_frequency_df, opponent_compatibility_df):
     """
     仮想敵（上位ポケモン）に対する有利度合いを計算する。
-    具体的には、攻めのタイプ相性を計算する。
+    具体的には、攻めのタイプ相性(打点数)を計算する。
     """
     # 二つのdfの列順をそろえる
     type_frequency_df = type_frequency_df[list(opponent_compatibility_df.columns[1:])]
@@ -18,8 +18,12 @@ def make_opponent_advantage_features(type_frequency_df, opponent_compatibility_d
         tmp_list = []
         for data in opponent_compatibility_df.itertuples():
             opponent_comatibility_raw = np.array(data[2:])
-            advantage_point = opponent_comatibility_raw.dot(party_type_count_raw)
-            
+            #advantage_point = opponent_comatibility_raw.dot(party_type_count_raw)
+            # 仮想敵に対する打点の数を計算する
+            # そのために一旦ベクトルの要素積を計算する
+            advantage_array = np.multiply(party_type_count_raw, opponent_comatibility_raw)
+            advantage_point = max([sum(advantage_array[advantage_array > 0]), 0])
+
             tmp_list.append(advantage_point)
         
         opponent_advantage_list.append(tmp_list)
@@ -27,6 +31,4 @@ def make_opponent_advantage_features(type_frequency_df, opponent_compatibility_d
     # 列名は"vsポケモン名"としてDataFrameを作成する
     opponent_advantage_df = pd.DataFrame(opponent_advantage_list, columns=list(map(lambda poke:"vs" + poke , opponent_compatibility_df["Unnamed: 0"])))
 
-    # 平均・標準偏差で正規化する
-    opponent_advantage_df = opponent_advantage_df.apply(lambda x: (x-x.mean())/ x.std(), axis=0)
     return opponent_advantage_df
--- kedro-project/src/kedro_project/pipelines/preprocess/node_merge_features.py
+++ kedro-project/src/kedro_project/pipelines/preprocess/node_merge_features.py
@@ -10,9 +10,15 @@ def merge_features(used_pokemon, type_frequency, opponent_advantage, params):
         merged = pd.concat([merged, used_pokemon], axis=1)
 
     if params["use_feature_type_frequency"]:
+        # type_frequency = type_frequency.apply(lambda x: (x-x.mean())/ x.std(), axis=0)
+        # [0, 1]で収まるように標準化する
+        type_frequency = type_frequency / type_frequency.max()
         merged = pd.concat([merged, type_frequency], axis=1)
 
     if params["use_feature_opponent_advantage"]:
+        # opponent_advantage = opponent_advantage.apply(lambda x: (x-x.mean())/ x.std(), axis=0)
+        # [0, 1]で収まるように標準化する
+        opponent_advantage = opponent_advantage / opponent_advantage.max()
         merged = pd.concat([merged, opponent_advantage], axis=1)
 
     return merged
