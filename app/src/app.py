8a6b7c93d2734e62c3efd60c4802438f77d9c943
ara-git <baribari.sanda@gmail.com>
2022-04-17 17:22:57 +0900

    update app.py

--- app/src/app.py
+++ app/src/app.py
@@ -5,8 +5,9 @@ import pandas as pd
 import streamlit as st
 import warnings
 import pickle
-import os
 from keras.models import load_model
+import yaml
+
 
 # 強引に上位ディレクトリ内のモジュールをインポートする
 import sys
@@ -14,22 +15,35 @@ sys.path.append('kedro_project/src/kedro_project/pipelines/preprocess')
 import node_make_used_pokemon_feature
 import node_make_used_type_feature
 import node_make_opponent_advantage_feature
+import node_merge_features
 
 warnings.simplefilter("ignore")
 
-# 学習済みモデルを読み込む
-model_gmbt = pickle.load(open('./kedro_project/data/06_models/trained_model_gbdt.pkl', 'rb'))
-model_cnn = load_model('./kedro_project/data/06_models/trained_model_cnn.h5')
+# タイトル
+st.title("ウーラオスの型判別AI")
 
-# 全ポケモンの名称が入ったリストを解凍する
+# 学習済みモデルを読み込む
+model_gbdt = pickle.load(open('./kedro_project/data/06_models/trained_model_gbdt.pkl', 'rb'))
+model_cnn = load_model('./kedro_project/data/06_models/trained_model_cnn.h5')
+model_logistic = pickle.load(open('./kedro_project/data/06_models/trained_model_logistic.pkl', 'rb'))
+
+# csv, yamlファイルを複数読み込む
+## 全ポケモンの名称が入ったリストを解凍する
 poke_data_sheet = pd.read_csv("./kedro_project/data/01_raw/pokemon_data_sheet.csv")
 poke_name_list = [None] + list(poke_data_sheet["名前"])
 
-# 高頻度ポケモンのリストを解凍する
+## 高頻度ポケモンのリストを解凍する
 frequent_pokemon_df = pd.read_csv("./kedro_project/data/02_intermediate/frequent_pokemon_list.csv")
 
-# 仮想敵のタイプ有利度に関するdfを解凍する
+## 仮想敵のタイプ有利度に関するdfを解凍する
 opponent_compatibility_df = pd.read_csv("./kedro_project/data/02_intermediate/opponent_compatibility.csv")
+
+## パラメータに関するymlファイルを解凍する
+with open('kedro_project/conf/base/parameters.yml', 'r', encoding= "utf-8") as yml:
+    parameters = yaml.safe_load(yml)
+
+## 使用する特徴量のリストを解凍する
+selected_feature_name_df = pd.read_csv("./kedro_project/data/01_raw/selected_features_name.csv")
 
 # ポケモン名を入力する
 input_poke_name_list = []
@@ -45,19 +59,52 @@ input_poke_name_list = list(filter(None, input_poke_name_list))
 # 重複を除く
 input_poke_name_list = list(set(input_poke_name_list))
 
-st.write(input_poke_name_list)
-
 if len(input_poke_name_list) == 5:
+    """
+    入力したポケモンの数が５体の時のみ実行する
+    """
+    # 一時敵に"悪ウーラオス"をリストに追加する
     input_poke_name_list.append("ウーラオス悪")
     input_poke_name_df = pd.DataFrame(input_poke_name_list).T
     input_poke_name_df.columns = ["p1", "p2", "p3", "p4", "p5", "p6"]
-    print(input_poke_name_df)
 
     # 特徴量を計算する
     used_pokemon_feature = node_make_used_pokemon_feature.make_used_pokemon_feature(input_poke_name_df, frequent_pokemon_df)
-    used_type_feature = node_make_used_type_feature.make_used_type_feature(input_poke_name_df, poke_data_sheet)
-    opponent_advantage_feature = node_make_opponent_advantage_feature.make_opponent_advantage_feature(used_type_feature, opponent_compatibility_df)
+    type_frequency_feature = node_make_used_type_feature.make_used_type_feature(input_poke_name_df, poke_data_sheet)
+    opponent_advantage_feature = node_make_opponent_advantage_feature.make_opponent_advantage_feature(type_frequency_feature, opponent_compatibility_df)
     
+    """
     st.write(used_pokemon_feature)
-    st.write(used_type_feature)
+    st.write(type_frequency_feature)
     st.write(opponent_advantage_feature)
+    """
+
+    #特徴量を結合し、"target"列だけ抜く
+    merged_features = node_merge_features.merge_features(used_pokemon_feature, type_frequency_feature, opponent_advantage_feature, selected_feature_name_df, parameters)
+    x = merged_features[merged_features.columns[merged_features.columns != "target"]]
+    
+    # 予測を行う
+    prediction_result_list = []
+
+    ## gbdt
+    pred_y_gbdt = model_gbdt.predict(x)
+    prediction_result_list.append(int(pred_y_gbdt))
+    
+    ## cnn
+    pred_y_cnn = model_cnn.predict(x)
+    prediction_result_list.append(round(float(pred_y_cnn)))
+    
+    ## logistic
+    pred_y_logistic = model_logistic.predict(x)
+    prediction_result_list.append(int(pred_y_logistic))
+
+    # 結果をまとめ、予測結果を出力する
+    prediction_result_df = pd.DataFrame(prediction_result_list).T
+    prediction_result_df.columns=["gbdt", "cnn", "logistic"]
+    prediction_result_df = prediction_result_df.replace({0: "水ウーラオス", 1: "悪ウーラオス"})
+    
+    st.write("ジャッジ１(light GBM gbdt)：", str(prediction_result_df["gbdt"][0]))
+    st.write("ジャッジ２(keras cnn)：", str(prediction_result_df["cnn"][0]))
+    st.write("ジャッジ３(sklearn logistic)：", str(prediction_result_df["logistic"][0]))
+
+
--- kedro_project/src/kedro_project/pipelines/preprocess/node_merge_features.py
+++ kedro_project/src/kedro_project/pipelines/preprocess/node_merge_features.py
@@ -10,15 +10,13 @@ def merge_features(used_pokemon, type_frequency, opponent_advantage, selected_fe
         merged = pd.concat([merged, used_pokemon], axis=1)
 
     if params["use_feature_type_frequency"]:
-        # type_frequency = type_frequency.apply(lambda x: (x-x.mean())/ x.std(), axis=0)
         # [0, 1]で収まるように標準化する
-        type_frequency = type_frequency / type_frequency.max()
+        # type_frequency = type_frequency / type_frequency.max()
         merged = pd.concat([merged, type_frequency], axis=1)
 
     if params["use_feature_opponent_advantage"]:
-        # opponent_advantage = opponent_advantage.apply(lambda x: (x-x.mean())/ x.std(), axis=0)
         # [0, 1]で収まるように標準化する
-        opponent_advantage = opponent_advantage / opponent_advantage.max()
+        # opponent_advantage = opponent_advantage / opponent_advantage.max()
         merged = pd.concat([merged, opponent_advantage], axis=1)
 
     # 使う特徴量を抽出する(パラメータでするかしないか決める)
